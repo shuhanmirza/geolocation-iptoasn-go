0f18dd5751a3fc4b4febd022629700ff1357e043
shuhanmirza <shuhan.mirza@gmail.com>
2023-04-27 15:06:03 +0600

    change directory of service test

--- geolink-go/api/service/main_test.go
+++ geolink-go/api/service/main_test.go
@@ -10,11 +10,11 @@ var keyValueStore *infrastructure.KeyValueStore
 var geoDataService GeoDataService
 
 func TestMain(t *testing.M) {
+	os.Chdir("../../")
+
 	keyValueStore = infrastructure.NewKeyValueStore()
 
 	geoDataService = NewGeoDataService(keyValueStore)
 
 	os.Exit(t.Run())
-
-	//Note: you need to change paths of the csv files in constant to run the tests. I am too lazy. Sorry.
 }
