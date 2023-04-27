1f3ac8dd57bf8f6650731f458214c478e3104e7c
shuhanmirza <shuhan.mirza@gmail.com>
2023-04-27 15:07:21 +0600

    comment out elaborate test of geoservice and implement proper log for v0 test

--- geolink-go/api/service/geodata_test.go
+++ geolink-go/api/service/geodata_test.go
@@ -2,50 +2,59 @@ package service
 
 import (
 	"geolink-go/api/structs"
-	"geolink-go/util"
-	"log"
-	"math/big"
-	"strconv"
-	"strings"
 	"testing"
 )
 
 func TestGeoDataService_GetIpGeoDataV0(t *testing.T) {
-	geoDataService.GetIpGeoData(structs.GetGeoDataRequest{
-		IpAddress: "1.0.16.1",
+	ipAddress := "103.84.159.230"
+	ipLocation := "BD"
+
+	data, err := geoDataService.GetIpGeoData(structs.GetGeoDataRequest{
+		IpAddress: ipAddress,
 	})
+	if err != nil {
+		t.Errorf("unexpected error %s", err.Error())
+	}
+	if data.CountryCode != ipLocation {
+		t.Errorf(" location of %s is %s but found %s", ipAddress, ipLocation, data.CountryCode)
+	}
 }
 
 func TestGeoDataService_GetIpGeoData(t *testing.T) {
 
-	records, _ := util.ReadCsvFile(util.GeoWhoisAsnCountryIpv4NumDatabaseFilename)
+	/*	commented out on purpose. uncomment when needed. this test requires more than 3 hours to complete
 
-	notFoundCount := big.NewInt(0)
-	failedCount := big.NewInt(0)
-	successfulCount := big.NewInt(0)
+		records, _ := util.ReadCsvFile(util.GeoWhoisAsnCountryIpv4NumDatabaseFilename)
 
-	for i, record := range records {
-		startIpInt, _ := strconv.Atoi(record[0])
-		endIpInt, _ := strconv.Atoi(record[1])
-		countryCode := record[2]
+		notFoundCount := big.NewInt(0)
+		failedCount := big.NewInt(0)
+		successfulCount := big.NewInt(0)
 
-		for ipInt := startIpInt; ipInt <= endIpInt; ipInt++ {
-			ipAddress := util.Int2ip(uint32(ipInt))
-			geoDataResponse, _ := geoDataService.GetIpGeoData(structs.GetGeoDataRequest{IpAddress: ipAddress.String()})
-			if geoDataResponse.CountryCode == "None" {
-				notFoundCount = notFoundCount.Add(notFoundCount, big.NewInt(1))
-			} else if strings.Compare(geoDataResponse.CountryCode, countryCode) != 0 {
-				failedCount = failedCount.Add(failedCount, big.NewInt(1))
+		for i, record := range records {
+			startIpInt, _ := strconv.Atoi(record[0])
+			endIpInt, _ := strconv.Atoi(record[1])
+			countryCode := record[2]
 
-				//log.Printf("failed | found=%s expected=%s\n", geoDataResponse.CountryCode, countryCode)
-			} else {
-				successfulCount = successfulCount.Add(successfulCount, big.NewInt(1))
+			for ipInt := startIpInt; ipInt <= endIpInt; ipInt++ {
+				ipAddress := util.Int2ip(uint32(ipInt))
+				geoDataResponse, _ := geoDataService.GetIpGeoData(structs.GetGeoDataRequest{IpAddress: ipAddress.String()})
+				if geoDataResponse.CountryCode == "None" {
+					notFoundCount = notFoundCount.Add(notFoundCount, big.NewInt(1))
+				} else if strings.Compare(geoDataResponse.CountryCode, countryCode) != 0 {
+					failedCount = failedCount.Add(failedCount, big.NewInt(1))
+
+					//log.Printf("failed | found=%s expected=%s\n", geoDataResponse.CountryCode, countryCode)
+				} else {
+					successfulCount = successfulCount.Add(successfulCount, big.NewInt(1))
+				}
 			}
+			log.Printf("batch completed | num %d | failed=%d | not found=%d | successful=%d\n", i, failedCount, notFoundCount, successfulCount)
 		}
-		log.Printf("batch completed | num %d | failed=%d | not found=%d | successful=%d\n", i, failedCount, notFoundCount, successfulCount)
-	}
 
-	log.Printf("test done | failed=%d | not found=%d | successful=%d\n", failedCount, notFoundCount, successfulCount)
+		log.Printf("test done | failed=%d | not found=%d | successful=%d\n", failedCount, notFoundCount, successfulCount)
 
-	//2023/04/20 02:55:03 test done | failed=120964971 | not found=625437929 | successful=2941251342
+		//2023/04/20 02:55:03 test done | failed=120964971 | not found=625437929 | successful=2941251342
+
+
+	*/
 }
